e2bd15aef19855537326b6830d84dc553d2491a7
JackKopperud <jack.kopperud@mnsu.edu>
2025-09-18 11:38:46 -0500

    Lab
    Lab work

---
+++ CIS-Lecture/Week3.py
@@ -0,0 +1,11 @@
+standards = int(input("Enter your standards:"))
+if standards >= 53:
+    print("A")
+elif standards >= 51:
+    print("A-")
+elif standards >= 49:
+    print("B+")
+elif standards >= 47:
+    print("B")
+elif standards >= 45:
+    print("B-")
